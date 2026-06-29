836d8d42631e40ad928bf462fcf04c89f2683f67
yasvmomen <yasvmomen@gmail.com>
2026-06-29 10:37:35 +0330

    tell how to work
    tell to jango how to find the urls in app files

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from website.views import http_test, json_test
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path ("url addres", "view", "")
+    path ('website/', include('website.urls')),
+    path ('', include('website.urls')),
+    path ('/', include('website.urls'))
 ]
+urlpatterns += static (settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+urlpatterns += static (settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
