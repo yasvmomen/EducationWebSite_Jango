656abbbbf4b837d7d8b34971ae13d73b565a1024
yasvmomen <yasvmomen@gmail.com>
2026-07-23 11:18:43 +0330

    Create Admin Plane
    add some features for admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from website.models import contact
+
+
+class contactAdmin (admin.ModelAdmin) :
+    date_hierarchy = 'created_date'
+    list_filter = ('name', 'email', 'subject')
+    search_fields = ('name', 'message', 'email', 'subject')
+    list_display = ('name', 'email', 'subject','created_date','updated_date')
+admin.site.register(contact, contactAdmin)
